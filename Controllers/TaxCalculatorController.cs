349a33a9d46f683b1b7862e0350d3b858c8ec2d4
Zenas Agada <agadazenas@gmail.com>
2025-05-14 22:47:36 +0100

    Added marginal bands to tax calculator

--- Controllers/TaxCalculatorController.cs
+++ Controllers/TaxCalculatorController.cs
@@ -23,8 +23,25 @@ namespace CVWebApp.Controllers
         private decimal CalculateTax(decimal income, string taxCode)
         {
             // Example logic for tax calculation
-            decimal taxRate = 0m; // Default tax rate
-            if (taxCode == "1257L") taxRate = 0m; // Example tax code
+            decimal taxRate = 0.2m; // Default tax rate
+            if (taxCode == "1257L")
+            {
+                if (income > 12570 && income < 55271)
+                {
+                    return (income - 12570) * 0.2m;
+                }
+                else if (income > 55270 && income < 100000)
+                {
+
+                    return (income - 55270) * 0.4m + 37700 * 0.2m;
+                }
+                else if (income > 100000 && income < 125140)
+                {
+                    decimal personalAllowance = (12570 - (125140 - income) / 2);
+                    return (income - 55270) * 0.4m + (55270 - personalAllowance) * 0.2m;
+                }
+                else if (income > 125140) return income * 0.45m;
+            }
             else if (taxCode == "BR") taxRate = 0.2m; // Basic rate
             else if (taxCode == "D0") taxRate = 0.4m; // Higher rate
             else if (taxCode == "D1") taxRate = 0.45m; // Additional rate
